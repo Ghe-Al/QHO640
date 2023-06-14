142358f00d56b894dda4ce4d049a732dc22c7c35
Ghe-Al <79513225+Ghe-Al@users.noreply.github.com>
2023-06-14 15:47:48 +0300

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ app.get('/*', (req, res) => {
 })
  
 app.post('/*', (req, res) => {
-  exec('php C:/Users/Administrator/Desktop/DC/Apps/calendar-app/src/php/api.php ' + req.params[0].split('/')[2] + ' ' + JSON.stringify(req.body).replace(/"/g, "'"), function (error, stdout, stderr) {console.log(stdout); res.send(stdout);});
+  exec('php ./php/api.php ' + req.params[0].split('/')[2] + ' ' + JSON.stringify(req.body).replace(/"/g, "'"), function (error, stdout, stderr) {console.log(stdout); res.send(stdout);});
 })
  
 app.listen(3001, () => {
